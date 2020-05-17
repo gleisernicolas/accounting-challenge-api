be01bd5b793513bf44ec366f954e12ea692dac68
Nicolas Gleiser <ncs.cr4sh@gmail.com>
2020-05-17 03:31:39 -0300

    create account model

---
+++ db/migrate/20200517061503_create_accounts.rb
@@ -0,0 +1,12 @@
+class CreateAccounts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :accounts do |t|
+      t.string :name, null: false
+      t.integer :balance, null: false
+      t.integer :account_number, null: false, unique: true
+      t.text :token, null: false, unique: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/accounts.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :account do
+    name { "MyString" }
+    balance { "999" }
+    token { "MyText" }
+  end
+end
